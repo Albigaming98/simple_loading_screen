c5be6fcaf11cc3777f07c84f0428450aa8c7c65f
Albigaming98 <91427364+Albigaming98@users.noreply.github.com>
2024-07-13 02:10:33 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Simple loadingscreen'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/music/loading_music.mp3',
+    'html/img/background.jpg'
+}
+
+loadscreen 'html/index.html'
